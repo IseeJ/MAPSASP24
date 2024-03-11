ec225f243790a1ee9f8f52276b2e8000f83aa660
IseeJ <122057875+IseeJ@users.noreply.github.com>
2024-03-11 13:23:41 -0400

    Create xmlcheck.py

---
+++ xmlcheck.py
@@ -0,0 +1,35 @@
+# $python3 xmlcheck.py QPT_35501_046R.xml
+
+import Utils
+import sys
+import xml.etree.ElementTree as ET
+
+
+def getID(sn):
+    db_accessor = Utils.DBaccess()
+    ID = db_accessor.component_id(sn,idt='serial_number')
+
+    return ID
+
+def checksn(allsn):
+    wrongsn=[]
+    for sn in allsn:
+        ID = getID(sn)
+        if ID is None:
+            wrongsn.append(sn)
+
+    return wrongsn
+
+def get_serial(filename):
+    path = "/uscms/home/jennetd/nobackup/outer-tracker/preproduction/XML/"+filename
+    tree = ET.parse(path)
+    root = tree.getroot()
+    serial_numbers = []
+    for part in root.findall('.//PART'):
+        serial = part.find('SERIAL_NUMBER')
+        if serial is not None:
+            serial_numbers.append(serial.text)
+    return serial_numbers
+
+#print(get_serial(sys.argv[1]))                                                                                  
+print(sys.argv[1]+"wrong sn:"+checksn(get_serial(sys.argv[1])))
